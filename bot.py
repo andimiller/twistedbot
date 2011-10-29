ff4f064cdbfbbe881c16ce9f0e0a8724b0b3f324
Andi Miller <andi@andimiller.net>
2011-10-29 20:27:17 +0100

    Refactored Logger into it's own class

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 from twisted.words.protocols import irc
 from twisted.internet import protocol
-from datetime import datetime
 from importer import Importer
-import pygments.console
+from logger import Logger
 import re
 
 class TwistedBot(irc.IRCClient):
@@ -22,45 +21,34 @@ class TwistedBot(irc.IRCClient):
     versionName = "TwistedBot"
     versionNum = "v0.1"
     sourceURL = "https://bitbucket.org/Sylnai/twistedbot/"
-
-    loglevels = {
-            0: "green",
-            1: "white",
-            2: "yellow",
-            3: "orange",
-            4: "red"
-            }
-
-    def _logit(self, loglevel, message):
-        m = "%s %s" % (datetime.now().strftime("%H:%M:%S"), message)
-        print pygments.console.colorize(self.loglevels[loglevel], m)
+    logger = Logger()
 
     def kickedFrom(self, channel, kicker, message):
-        self._logit(4,"Kicked from %s by %s with message %s" % (channel, kicker, message))
+        self.logger.log(4,"Kicked from %s by %s with message %s" % (channel, kicker, message))
 
     def userJoined(self, user, channel):
-        self._logit(2,"%s joined %s" % (user, channel))
+        self.logger.log(2,"%s joined %s" % (user, channel))
 
     def userKicked(self, kickee, channel, kicker, message):
-        self._logit(3,"%s got kicked from %s by %s with message %s" % (kickee, channel, kicker, message))
+        self.logger.log(3,"%s got kicked from %s by %s with message %s" % (kickee, channel, kicker, message))
         for f in self.userKickedFunctions:
             f(self, kickee, channel, kicker, message)
 
     def signedOn(self):
         self.join(self.factory.channel)
-        self._logit(0,"Signed on as %s." % (self.nickname))
+        self.logger.log(0,"Signed on as %s." % (self.nickname))
 
     def joined(self, channel):
-        self._logit(0,"Joined %s." % (channel))
+        self.logger.log(0,"Joined %s." % (channel))
         for j in self.joinedFunctions:
             j(self, channel)
 
     def privmsg(self, user, channel, msg):
         user= user.split("!")[0]
-        self._logit(1,"%s: <%s> %s" % (channel,user,msg))
+        self.logger.log(1,"%s: <%s> %s" % (channel,user,msg))
         for r in self.functions.keys():
             if r.match(msg):
-                self._logit(0,"Launching: %s" % self.functions[r])
+                self.logger.log(0,"Launching: %s" % self.functions[r])
                 self.functions[r](self, user, channel, msg)
 
 class TwistedBotFactory(protocol.ClientFactory):

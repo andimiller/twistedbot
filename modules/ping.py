45a3442d20766e909f35b8f12e1b7bf2e818e4e4
Andi Miller <andi@andimiller.net>
2011-10-29 22:30:10 +0100

    Made a regex more readable in ping

--- modules/ping.py
+++ modules/ping.py
@@ -19,7 +19,7 @@ def question(tbot, user, channel, msg):
    message = user+": " 
    message = message + random.choice(('As I see it, yes','It is certain','It is decidedly so','Most likely','Outlook good','Signs point to yes','Without a doubt','Yes','Yes, definitely','You may rely on it','Reply hazy, try agan','Ask again later','Better not tell you now','Cannot predict now','Concentrate and ask again','Dont count on it','My reply is no','My sources say no','Outlook not so good','Very doubtful'))
    tbot.msg(channel,message)
-question.rule = 'TwistedBot([A-Za-z0-9 ,.-:\']*)(should|can|Should|Can)([A-Za-z0-9 ,.\-?]*\?)'
+question.rule = 'TwistedBot: (should|can|Should|Can) .*\?)'
 
 def basara(tbot, user, channel, msg):
    tbot.msg(channel,"Yukimura!")

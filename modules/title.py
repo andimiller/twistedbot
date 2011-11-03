1da654e001e1338526aa87911cf1a849d0bec85e
Andi Miller <andi@andimiller.net>
2011-11-03 21:28:50 +0000

    Improved the title module

--- modules/title.py
+++ modules/title.py
@@ -2,13 +2,18 @@ import urllib2, re
 from BeautifulSoup import BeautifulSoup
 
 removetags=re.compile("<(.|\n)*?>")
+collapsespaces=re.compile(" +")
 
 def gettitle(url):
     response = urllib2.urlopen(url)
     data = response.read()
     soup = BeautifulSoup(data)
     result = unicode(soup.find("title"))
-    return removetags.sub("",result)
+    result = removetags.sub("",result)
+    result = result.replace("\n", "")
+    result = collapsespaces.sub(" ", result)
+    result = result.strip()
+    return result
 
 def title(tbot, user, channel, msg):
     msg = msg.split(" ")

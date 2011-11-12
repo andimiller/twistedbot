24bfff764a6a704809eac44416eff3a99d299e3e
Andi Miller <andi@andimiller.net>
2011-11-12 01:25:29 +0000

    Initial regex functionality.

--- modules/regex.py
+++ modules/regex.py
@@ -1,3 +1,14 @@
+import re
+
+def sub(message, regex):
+    regex=re.split("(?<!\\\\)/",regex)
+    return re.sub(regex[1], regex[2], message)
+
+def substitute(tbot, user, channel, msg):
+    tbot.logger.log("INFO", "Regexing:%s" % msg)
+    tbot.msg(channel, sub(tbot.messages[user], msg))
+substitute.rule="^s\/.*"
+
 def lastmsg(tbot, user, channel, msg):
     msg = msg.split()
     if len(msg)>1 and msg[1] in tbot.messages:
@@ -7,5 +18,6 @@ lastmsg.rule="^!lastmsg"
 def storemessage(tbot, user, channel, msg):
     if not hasattr(tbot, "messages"):
         tbot.messages = dict()
-    tbot.messages[user]=msg
+    if not msg.startswith("s/"):
+        tbot.messages[user]=msg
 storemessage.rule=".*"

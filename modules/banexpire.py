4aa4e6b4a4e0d29adeca66c00b5c7d02d114455b
Andi Miller <andi@andimiller.net>
2012-01-01 21:44:24 +0000

    Added some terribly unfunny ban responses.

--- modules/banexpire.py
+++ modules/banexpire.py
@@ -1,5 +1,14 @@
 from twisted.internet import protocol
+from random import choice
 from twisted.internet import task
+
+banresponses = [
+        "Yo dawg, I pity the fool who thinks they be an admin. Your ban will be removed in 30 seconds.",
+        "Stop right there criminal scum. Your ban will be removed in 30 seconds.",
+        "I used to make bans like you, but then I took an arrow to the... face. As will you in 30 seconds.",
+        "I see what you did there, and I'll be reverting it in 30 seconds."
+        ]
+
 
 def bandetect(tbot, user, channel, set, modes, args):
     nick = user.split("!")[0]
@@ -9,7 +18,7 @@ def bandetect(tbot, user, channel, set, modes, args):
             return
         for index,b in enumerate(modes):
             tbot.logger.log("INFO", "Saw a ban by %s" % user)
-            tbot.say(channel, "%s: Yo dawg, I pity the fool who thinks he an admin, I'll be removing that ban in 30 seconds." % nick)
+            tbot.say(channel, "%s: %s" % (nick,choice(banresponses)))
             d = tbot.reactor.callLater(30, removeban, tbot, channel, args[index])
             #tbot.mode(channel, False, "-b %s" % args[index])
 bandetect.modeChanged = True

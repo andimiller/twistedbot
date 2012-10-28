c026bfcddcf4b8d52957cf3e799fe4802a5df94e
Louis Taylor <kragniz@gmail.com>
2012-10-28 18:47:59 +0000

    Updated the ponies

--- modules/ponycountdown.py
+++ modules/ponycountdown.py
@@ -12,7 +12,16 @@ def nextponies():
             (datetime(2012,  1, 14, 15),2,13,"Baby Cakes"),
             (datetime(2012,  1, 21, 15),2,14,"The last Roundup"),
             (datetime(2012,  1, 28, 15),2,15,"The Super Speedy Cider Squeezy 6000"),
-            (datetime(2012,  2, 4,  15),2,16,"Read It and Weep")
+            (datetime(2012,  2, 4,  15),2,16,"Read It and Weep"),
+            
+            (datetime(2012, 11, 10, 15),3,1,"The Crystal Empire, Part 1"),
+            (datetime(2012, 11, 10, 16),3,2,"The Crystal Empire, Part 2"),
+            (datetime(2012, 11, 17, 15),3,3,"Too Many Pinkie Pies"),
+            (datetime(2012, 11, 24, 15),3,4,"One Bad Apple"),
+            (datetime(2012, 12, 3, 15),3,5,"Magic Duel"),
+            (datetime(2012, 12, 10, 15),3,6,"Sleepless in Ponyville"),
+            (datetime(2012, 12, 17, 15),3,7,"Wonderbolt Academy"),
+            (datetime(2012, 12, 24, 15),3,8,"Apple Family Reunion")
             ]
     r=map(lambda x:(x[0]-datetime.now(),x[1],x[2],x[3]), times)
     r=sorted(r)
